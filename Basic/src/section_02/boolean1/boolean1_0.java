02d7891202d28f8eafd74dc96da934309570d28b
Ion Kim <yyyy.oniiii@gmail.com>
2021-05-09 18:47:55 +0900

    Java Day03

---
+++ Basic/src/section_02/boolean1/boolean1_0.java
@@ -0,0 +1,21 @@
+/*
+ * Date: 2021-05-09
+ * Author: Yon Kim
+ * Project: [자바 초보탈출 30일]
+ * Section: 03일차
+ * Subject: "논리 자료형"
+ */
+
+
+package section_02.boolean1;
+
+public class boolean1_0 {
+    public static void main(String[] args){
+        boolean isMarried = true;       // boolean 변수를 선언하고 초기화
+        System.out.println(isMarried);  // 결과 값은 true, false로만 출력
+
+        boolean isMarried2 = false;
+        System.out.println(isMarried2);
+    }
+
+}
