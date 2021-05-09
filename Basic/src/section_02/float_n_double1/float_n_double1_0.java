00a73dd648cecc8a7d03eb166c3c2ea0d327ce49
Ion Kim <yyyy.oniiii@gmail.com>
2021-05-09 18:36:54 +0900

    Java Day03

---
+++ Basic/src/section_02/float_n_double1/float_n_double1_0.java
@@ -0,0 +1,20 @@
+/*
+ * Date: 2021-05-09
+ * Author: Yon Kim
+ * Project: [자바 초보탈출 30일]
+ * Section: 03일차
+ * Subject: "실수 자료형"
+ */
+
+
+package section_02.float_n_double1;
+
+    public class float_n_double1_0 {
+        public static void main(String[] arg) {
+            double dnum = 3.14; // Java에서 실수는 double 형을 기본으로 사용
+            float fnum = 3.14F; // Float은 식별자 F로 구분
+
+            System.out.println(dnum);
+            System.out.println(fnum);
+        }
+}
