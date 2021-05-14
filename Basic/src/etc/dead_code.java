0f7ab8b5eb4d94709ab58d53e654e18f3241e2b0
Ion Kim <yyyy.oniiii@gmail.com>
2021-05-14 22:10:44 +0900

    add etc directory

---
+++ Basic/src/etc/dead_code.java
@@ -0,0 +1,24 @@
+package etc;
+
+    /*
+     * dead_code
+     */
+
+    public class dead_code {
+        public static void main(String[] args) {
+            
+            int num1 = 1;   // Dead Code (선언 후 사용되지 않은 변수)
+
+            int num2 = 2;
+            System.out.println(num2);
+
+            if (num2 > 1) {
+                System.out.println('T');
+            }
+
+            else {                        // Dead Code
+                System.out.println('F');  // (조건문의 결과가 항상 'T' 이므로)
+            }
+        }
+        
+    }
